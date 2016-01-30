308d6824de202ff8f44a18035f73d78e2f62fd89
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-29 20:29:38 -0500

    meme generator completed

--- meme-generator/app.js
+++ meme-generator/app.js
@@ -4,8 +4,23 @@
 
 var main = function() {
 
+    $( "#top-text" ).keyup(function() {
+        var text_value = $("#top-text").val()
+        $('h1.top-caption').text(text_value);
+    });
+
+    $( "#bottom-text" ).keyup(function() {
+        var text_value = $("#bottom-text").val()
+        $('h1.bottom-caption').text(text_value);
+    });
+
+    $( "#image-url" ).keyup(function() {
+        var text_value = $("#image-url").val()
+        $('img').attr('src', text_value);
+    });
+
+
 }
 
 $(document).ready(main);
 
-s
