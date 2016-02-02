0a0e1793fe7d110319aebd1f53205a0191ed499e
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-02-01 21:00:48 -0500

    toggling and adding the click event handler

---
+++ Birdman/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by rakesh on 2/1/16.
+ */
+
+var main = function() {
+    $('img').click( function() {
+        $('.dropdown-menu').toggle();
+        // toggling the drop down menu
+    });
+}
+$(document).ready(main);
--- meme-generator/app.js
+++ meme-generator/app.js
@@ -1,7 +1,6 @@
 /**
  * Created by rakesh on 1/29/16.
  */
-
 var main = function() {
 
     $( "#top-text" ).keyup(function() {
