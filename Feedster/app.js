37f2294ff5f2dd05c563dbe27f0a89454c514e2d
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-02-01 22:25:46 -0500

    feed ster is done

--- Feedster/app.js
+++ Feedster/app.js
@@ -1,4 +1,12 @@
 var main = function() {
+    $('.notification img').click(function() {
+        $('.notification-menu').toggle();
+
+    });
+
+    $('.btn').click(function() {
+        $(this).toggleClass('btn-like');
+    });
 
 }
 
